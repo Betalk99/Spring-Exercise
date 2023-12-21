837540b51b2d1f979ee392317983dc42e2dc5780
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2023-12-21 10:38:38 +0100

    add folders Example

---
+++ Sample/demo-webapp/src/main/java/com/example/demowebapp/HelloWorldControllelr.java
@@ -0,0 +1,18 @@
+package com.example.demowebapp;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
+public class HelloWorldControllelr {
+    @RequestMapping(method = RequestMethod.GET, path = "/helloWorld")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    @GetMapping(path = "/ciao")
+    public String ciao(@RequestParam(value = "pippo", required = false, defaultValue = "Bruno") String nome, @RequestParam(required = false) String cognome){
+        return "Hello " + nome + " " + cognome;
+    }
+
+}
