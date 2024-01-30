71ec336ce1e89043536c737daf043206aae8806b
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2024-01-30 16:37:44 +0100

    add class

---
+++ Spring Advance/API-Interceptor-Middleware-01/src/main/java/com/bruno/API/Interceptor/Middleware1/SpringMvcConfig.java
@@ -0,0 +1,7 @@
+package com.bruno.API.Interceptor.Middleware1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Component
+public class SpringMvcConfig implements WebMvcConfigurer {
+}
